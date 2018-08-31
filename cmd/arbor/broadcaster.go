74f34ae0213f22a29f7221ca04b30c8f24d8cd3d
Christopher Waldon <christopher.waldon.dev@gmail.com>
2018-08-30 22:09:22 -0400

    Fix broadcaster logic to not use closures

--- cmd/arbor/broadcaster.go
+++ cmd/arbor/broadcaster.go
@@ -20,28 +20,37 @@ func NewBroadcaster() *Broadcaster {
 
 func (b *Broadcaster) Send(message *messages.ArborMessage) {
 	b.RLock()
+	log.Println("Send acquired rlock")
 	for client := range b.clients {
-		go func() {
-			defer func() {
-				if err := recover(); err != nil {
-					log.Println("Error sending to client, removing: ", err)
-					go b.remove(client)
-				}
-			}()
-			client <- message
-		}()
+		go b.trySend(message, client)
 	}
 	b.RUnlock()
+	log.Println("Send released rlock")
+}
+
+func (b *Broadcaster) trySend(message *messages.ArborMessage, client chan<- *messages.ArborMessage) {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println("Error sending to client, removing: ", err)
+			go b.remove(client)
+		}
+	}()
+	log.Println("trying send to ", client)
+	client <- message
 }
 
 func (b *Broadcaster) remove(client chan<- *messages.ArborMessage) {
 	b.Lock()
+	log.Println("removed acquired lock")
 	delete(b.clients, client)
 	b.Unlock()
+	log.Println("remove released lock", client)
 }
 
 func (b *Broadcaster) Add(client chan<- *messages.ArborMessage) {
 	b.Lock()
+	log.Println("Add acquired lock")
 	b.clients[client] = struct{}{}
 	b.Unlock()
+	log.Println("Add released lock")
 }
