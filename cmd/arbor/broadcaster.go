ea3e77e915aa72f9bb5362ef277214822323a721
Christopher Waldon <christopher.waldon.dev@gmail.com>
2018-09-05 23:44:30 -0400

    Remove mutex from broadcaster logic

--- cmd/arbor/broadcaster.go
+++ cmd/arbor/broadcaster.go
@@ -2,55 +2,59 @@ package main
 
 import (
 	"log"
-	"sync"
 
 	"github.com/whereswaldon/arbor/lib/messages"
 )
 
 type Broadcaster struct {
-	sync.RWMutex
-	clients map[chan<- *messages.ArborMessage]struct{}
+	send       chan *messages.ArborMessage
+	disconnect chan chan<- *messages.ArborMessage
+	connect    chan chan<- *messages.ArborMessage
+	clients    map[chan<- *messages.ArborMessage]struct{}
 }
 
 func NewBroadcaster() *Broadcaster {
-	return &Broadcaster{
-		clients: make(map[chan<- *messages.ArborMessage]struct{}),
+	b := &Broadcaster{
+		send:       make(chan *messages.ArborMessage),
+		connect:    make(chan chan<- *messages.ArborMessage),
+		disconnect: make(chan chan<- *messages.ArborMessage),
+		clients:    make(map[chan<- *messages.ArborMessage]struct{}),
+	}
+	go b.dispatch()
+	return b
+}
+
+func (b *Broadcaster) dispatch() {
+	for {
+		select {
+		case message := <-b.send:
+			for client := range b.clients {
+				go b.trySend(message, client)
+			}
+		case newclient := <-b.connect:
+			b.clients[newclient] = struct{}{}
+
+		case deadclient := <-b.disconnect:
+			delete(b.clients, deadclient)
+		}
 	}
 }
 
 func (b *Broadcaster) Send(message *messages.ArborMessage) {
-	b.RLock()
-	log.Println("Send acquired rlock")
-	for client := range b.clients {
-		go b.trySend(message, client)
-	}
-	b.RUnlock()
-	log.Println("Send released rlock")
+	b.send <- message
 }
 
 func (b *Broadcaster) trySend(message *messages.ArborMessage, client chan<- *messages.ArborMessage) {
 	defer func() {
 		if err := recover(); err != nil {
 			log.Println("Error sending to client, removing: ", err)
-			go b.remove(client)
+			b.disconnect <- client
 		}
 	}()
 	log.Println("trying send to ", client)
 	client <- message
 }
 
-func (b *Broadcaster) remove(client chan<- *messages.ArborMessage) {
-	b.Lock()
-	log.Println("removed acquired lock")
-	delete(b.clients, client)
-	b.Unlock()
-	log.Println("remove released lock", client)
-}
-
 func (b *Broadcaster) Add(client chan<- *messages.ArborMessage) {
-	b.Lock()
-	log.Println("Add acquired lock")
-	b.clients[client] = struct{}{}
-	b.Unlock()
-	log.Println("Add released lock")
+	b.connect <- client
 }
